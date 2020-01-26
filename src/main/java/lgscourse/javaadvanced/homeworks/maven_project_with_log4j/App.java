a40531bd0e36f19e8be5249e44960634147c1d92
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-01-26 21:43:45 +0200

    initial commit

---
+++ src/main/java/lgscourse/javaadvanced/homeworks/maven_project_with_log4j/App.java
@@ -0,0 +1,13 @@
+package lgscourse.javaadvanced.homeworks.maven_project_with_log4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
