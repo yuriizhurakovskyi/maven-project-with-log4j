0e17a3ee35ca2cddbca01a8dac4d8cf8aab5c776
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-01-26 22:59:11 +0200

    add log4j.xml, logger object and few logs

--- src/main/java/lgscourse/javaadvanced/homeworks/maven_project_with_log4j/App.java
+++ src/main/java/lgscourse/javaadvanced/homeworks/maven_project_with_log4j/App.java
@@ -1,13 +1,18 @@
 package lgscourse.javaadvanced.homeworks.maven_project_with_log4j;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class App {
+	private static Logger logger = Logger.getLogger(App.class);
+
+	public static void main(String[] args) {
+		DOMConfigurator.configure("log4j.xml");
+		logger.debug("Debug Message!");
+		logger.info("Info Message!");
+		logger.trace("Trace Message!");
+		logger.warn("Warn Message!");
+		logger.error("Error Message!");
+		logger.fatal("Fatal Error Message!");
+	}
 }
